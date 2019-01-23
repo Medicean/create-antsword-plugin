9424335c0f7c8f18c38ed317cbecf568325ac3d0
Medicean <Medici.Yan@Gmail.com>
2019-01-23 17:01:49 +0800

    add language file

---
+++ language/en.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: "Create AntSword Plugin",
+    success: "Create Success",
+    error: "Create Error",
+    warning: "Please complete the form",
+    toolbar: {
+        create: "Create",
+        choose: "Confirm",
+    },
+    form: {
+        title: 'Plugin Config',
+        name: 'Name(Chinese)',
+        name_en: 'Name(English)',
+        icon: 'Icon',
+        chooseicon: 'Choose Icon',
+        selectedicon: 'Selected:',
+        description: "Description(Chinese)",
+        description_en: "Description(English)",
+        category: "Category",
+        multiple: "Multiple",
+        scripts: "Scripts",
+    },
+    chooseicon: {
+        notselect: 'Please choose an icon',
+        success: 'Operation success',
+    },
+    confirm: {
+        title: 'Info',
+        content: 'Restart the application and load the new plugin?',
+    },
+}
---
+++ language/zh.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: "创建插件",
+    success: "创建成功",
+    error: "创建失败",
+    warning: "请填写完整",
+    toolbar: {
+        create: "创建",
+        choose: "选择",
+    },
+    form: {
+        title: '插件配置',
+        name: '名称(中文)',
+        name_en: '名称(英文)',
+        icon: '图标',
+        chooseicon: '选择图标',
+        selectedicon: '已选择:',
+        description: "描述",
+        description_en: "描述(英文)",
+        category: "分类",
+        multiple: "可批量调用",
+        scripts: "支持脚本",
+    },
+    chooseicon: {
+        notselect: '请先选择一个图标',
+        success: '操作成功',
+    },
+    confirm: {
+        title: '提示',
+        content: '重启应用即可加载新插件,是否重启',
+    },
+}
